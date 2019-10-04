3ed4f0692bba0fedca1afb0fc7deace16ffa8546
hoffmantim <hoffmantim@me.com>
2019-10-04 12:22:39 -0500

    added some styling: background color, link colors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
+    var navBar = document.getElementById("myTopnav");
+    if (navBar.className === "topnav") {
+      navBar.className += " responsive";
     } else {
-      x.className = "topnav";
+      navBar.className = "topnav";
     }
   } 
