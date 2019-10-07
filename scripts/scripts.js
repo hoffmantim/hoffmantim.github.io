d26db9a2e9a16e30f9d4653173d517245df423db
hoffmantim <hoffmantim@me.com>
2019-10-07 07:48:17 -0500

    fixed heading text on smaller screens; added course description page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,13 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var navBar = document.getElementById("myTopnav");
+    const navBar = document.getElementById("myTopnav");
+    const title = document.getElementById("title-text");
+
     if (navBar.className === "topnav") {
       navBar.className += " responsive";
+      title.className += " hidden"
     } else {
       navBar.className = "topnav";
+      title.className = "title-text"
     }
-  } 
+  };
